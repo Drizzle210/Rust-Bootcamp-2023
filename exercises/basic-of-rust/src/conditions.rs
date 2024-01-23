a41eb826fd0961e73a1f3ddd651a3eff47a617f3
Drizzle210 <anhduc5a1tp@gmail.com>
2024-01-23 13:42:44 +0700

    done lesson-1-excercises

--- exercises/basic-of-rust/src/conditions.rs
+++ exercises/basic-of-rust/src/conditions.rs
@@ -4,15 +4,25 @@
 // - another function call
 // - additional variables
 pub fn bigger(a: i32, b: i32) -> i32 {
-    todo!()
+    if a >= b {
+        a
+    }
+    else {
+        b
+    }
 }
 
 //Exercise 2
 // Input: Provide an arbitrary value of number
 // Check number is Positive or Negative or Zero
 // Output: &str
-fn check_number(number: u32) -> &'static str {
-    todo!()
+fn check_number(number: i32) -> &'static str {
+    match number {
+        0 => "Zero",
+        n if n > 0 => "Positive",
+        n if n < 0 => "Negative",
+        _ => unreachable!(),
+    }
 }
 
 // Exercise 3
@@ -23,7 +33,7 @@ pub fn foo_if_fizz(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
     } else {
-        1
+        "1"
     }
 }
 
@@ -31,14 +41,23 @@ pub fn foo_if_fizz(fizzish: &str) -> &str {
 // Determine if a given year is a leap year
 // Implement logic
 fn is_leap_year(year: i32) -> bool {
-    todo!()
+    if year % 4 == 0 && year % 100 != 0 {
+        true
+    }
+    else {
+        false
+    }
 }
 
 // Exercise 5
 // Calculate the factorial of a number
 // Implement logic
 fn factorial(n: u32) -> u32 {
-    todo!()
+    let mut res = 1;
+    for i in 1..=n {
+        res *= i;
+    }
+    res
 }
 
 // Exercise 6
@@ -46,8 +65,20 @@ fn factorial(n: u32) -> u32 {
 // Implement logic
 
 fn is_prime(n: u32) -> bool {
-    todo!()
+    if n < 2 {
+        return false;
+    }
+
+    let mut cnt = 0;
+    for i in 2..=n {
+        if n % i == 0 {
+            cnt += 1;
+        }
+    }
+
+    cnt == 1 // A prime number has only two divisors: 1 and itself
 }
+
 
 
 // Don't mind this for now :)
--- exercises/basic-of-rust/src/functions.rs
+++ exercises/basic-of-rust/src/functions.rs
@@ -1,33 +1,67 @@
 // Exercise 1
-// Fix all errors 
-fn sum(x, y: i32) {
-    x + y;
-}
-
-//Exercise 2
-// Input: Provide an arbitrary value of n
-// Implement sum function: 1+2+3+..n
-// Output: Calculate sum 1 to n 
-pub fn sum_one_to_n(n: u32) -> u32 {
-    // your code for summing all digits from 1 to `n` (inclusive) should go
-    // here (you can remove the sample return of `0`)
-    0
-}
-
-// Exercise 3
-// Input: list of arbitrary numbers
-// Problem: Calculate the average of a list of numbers
-// Output: Average Number 
-fn calculate_average(numbers: &[f64]) -> f64 {
+#[allow(dead_code)]
+fn exercise1(color: &str) -> String {
     todo!()
+}
+
+// Exercise 2
+// Fix all errors without adding newline
+fn exercise2() -> String {
+    let mut s = String::from("hello");
+    s.push(',');
+    s += " world";
+    s += "!";
+    s
+}
+// Exercise 3
+// Fix errors without removing any line
+fn exercise3() -> String {
+    let s1 = String::from("hello,");
+    let s2 = String::from("world!");
+    let s3 = s1 + s2.as_str();
+    s3
 }
 
 // Exercise 4
-// Calculate the sum of all even numbers in a list
-fn sum_even_numbers(numbers: &[i32]) -> i32 {
-    todo!()
+// Reverse a string
+
+fn reverse_string(input: &str) -> String {
+    let mut res = String::new();
+    let n = input.len();
+    for i in (0..n).rev() {
+        res.push(input.chars().nth(i).unwrap());
+    }
+    res
 }
 
+
+// Exercise 5
+// Check if a string is a palindrome
+fn is_palindrome(word: &str) -> bool {
+    let n = word.len();
+    for i in 0..n / 2 {
+        let c1 = word.chars().nth(i).unwrap();
+        let c2 = word.chars().nth(n - i - 1).unwrap();
+        if c1 != c2 {
+            return false;
+        }
+    }
+    true
+}
+
+// Exercise 6
+// Count the occurrences of a character in a string
+fn count_char_occurrences(string: &str, ch: char) -> usize {
+    let mut res:usize = 0;
+    let n = string.len();
+    for i in 0..n {
+        let c = string.chars().nth(i).unwrap();
+        if c == ch {
+            res += 1;
+        }
+    }
+    res
+}
 
 #[cfg(test)]
 mod tests {
@@ -35,63 +69,52 @@ mod tests {
 
     // Test for exercise 1
     #[test]
-    fn sum_should_work() {
-        let (x, y) = (1, 2);
-        let s = sum(x, y);
+    fn exercise1_work() {
+        assert_eq!("white".to_string(), exercise1("white"));
+    }
+
+    // Test for exercise 2
+    #[test]
+    fn exercise2_work() {
+        assert_eq!("hello, world!".to_string(), exercise2());
+    }
+
+    // Test for exercise 3
+    #[test]
+    fn exercise3_work() {
+        assert_eq!("hello, world!".to_string(), exercise3());
+    }
     
-        assert_eq!(s, 3);
-    }
-
-    // Test for exercise 2
-    #[test]
-    fn test_sum_0() {
-        let result = sum_one_to_n(0);
-
-        assert_eq!(result, 0);
-    }
-
-    // Test for exercise 2
-    #[test]
-    fn test_sum_1() {
-        let result = sum_one_to_n(1);
-
-        assert_eq!(result, 1);
-    }
-
-    // Test for exercise 2
-    #[test]
-    fn test_sum_100() {
-        let result = sum_one_to_n(100);
-
-        assert_eq!(result, 5050);
-    }
-
-    // Test for exercise 3
-    #[test]
-    fn test_calculate_average() {
-        // Test case 1: Non-empty slice
-        let numbers = [2.5, 4.8, 6.3, 1.7, 3.9];
-        let result = calculate_average(&numbers);
-        assert_eq!(result, 3.84);
-
-    }
-
-    // Test for exercise 3
-    #[test]
-    fn test_calculate_average_empty() {
-        // Test case 1: Non-empty slice
-        let numbers = [];
-        let result = calculate_average(&numbers);
-        assert_eq!(result, 0.0);
-
-    }
-
     // Test for exercise 4
     #[test]
-    fn test_sum_even_numbers() {
-        assert_eq!(sum_even_numbers(&[1, 2, 3, 4, 5, 6]), 12);
-        assert_eq!(sum_even_numbers(&[10, 20, 30, 40, 50]), 150);
-        assert_eq!(sum_even_numbers(&[15, 25, 35, 45, 55]), 0);
-        assert_eq!(sum_even_numbers(&[-2, 0, 2, 4, 6]), 10);
+    fn test_reverse_string() {
+        assert_eq!(reverse_string("hello"), "olleh");
+        assert_eq!(reverse_string("rust"), "tsur");
+        assert_eq!(reverse_string("world"), "dlrow");
+        assert_eq!(reverse_string(""), "");
     }
+
+    // Test for exercise 5
+    #[test]
+    fn test_palindrome() {
+        assert_eq!(is_palindrome("level"), true);
+        assert_eq!(is_palindrome("deed"), true);
+        assert_eq!(is_palindrome("Rotor"), true);
+    }
+    // Test for exercise 5
+    #[test]
+    fn test_non_palindrome() {
+        assert_eq!(is_palindrome("hello"), false);
+        assert_eq!(is_palindrome("world"), false);
+    }
+
+    // Test for exercise 6
+
+    #[test]
+    fn test_count_char_occurrences() {
+        assert_eq!(count_char_occurrences("Hello", 'l'), 2);
+        assert_eq!(count_char_occurrences("Rust is fun", 'u'), 1);
+        assert_eq!(count_char_occurrences("Mississippi", 's'), 4);
+    }
+
 }
