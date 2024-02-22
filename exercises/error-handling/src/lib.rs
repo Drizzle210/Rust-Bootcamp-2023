419e04b4aaaee31d8e2c63d8f0b4e19a29cf3f2e
Drizzle210 <anhduc5a1tp@gmail.com>
2024-02-22 17:08:34 +0700

    done class-5-exercises

--- exercises/error-handling/src/lib.rs
+++ exercises/error-handling/src/lib.rs
@@ -2,12 +2,11 @@
 // Make it compile in unit test
 // Run tests
 // Hint: Convert Option to Result
-fn generate_nametag_text(name: String) -> Option<String> {
+fn generate_nametag_text(name: String) -> Result<String , String> {
     if name.is_empty() {
-        // Empty names aren't allowed.
-        None
+        Err("`name` was empty; it must be nonempty.".to_string())
     } else {
-        Some(format!("Hi! My name is {}", name))
+        Ok(format!("Hi! My name is {}", name))
     }
 }
 // Exercise 2
@@ -17,7 +16,8 @@ fn generate_nametag_text(name: String) -> Option<String> {
 use std::num::ParseIntError;
 
 fn parse_number(s: &str) -> Result<i32, ParseIntError> {
-    todo!()
+    let parse_number:Result<i32 , ParseIntError> = s.parse();
+    parse_number
 }
 
 // Exercise 3
@@ -35,8 +35,21 @@ enum CreationError {
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
-        // Hmm...? Why is this only returning an Ok value?
-        Ok(PositiveNonzeroInteger(value as u64))
+        match value {
+            0 => {
+                Err(CreationError::Zero)
+            }
+            _ => {
+                if value < 0 {
+                    Err(CreationError::Negative)
+                }
+                else {
+                    Ok(PositiveNonzeroInteger(value as u64))
+                }
+            }
+        }
+        
+        
     }
 }
 
@@ -63,10 +76,7 @@ mod tests {
     #[test]
     fn exercise2_should_work() {
         assert_eq!(parse_number("42"), Ok(42));
-        assert_eq!(
-            parse_number("invalid"),
-            Err("invalid digit found in string".parse().unwrap())
-        );
+        assert!(parse_number("invalid").is_err());
     }
 
     /// Test for exercise 3
